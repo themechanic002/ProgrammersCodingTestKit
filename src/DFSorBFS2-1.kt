8a0f7eefa497c96fdda5745ac95e2614d35bc6e4
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-13 20:36:27 +0900

    commit

--- src/DFSorBFS2-1.kt
+++ src/DFSorBFS2-1.kt
@@ -13,12 +13,16 @@ class Solution19 {
     fun solution(tickets: Array<Array<String>>): ArrayList<String> {
         var answer = ArrayList<String>()
 
-        //티켓 중복 제거
-        val ticketsSet = ArrayList<ArrayList<String>>()
-        for (i in tickets)
-            ticketsSet.add(arrayListOf(i[0], i[1]))
+        val allAirport = ArrayList<String>()
+        allAirport.add("ICN")
+        tickets.forEach { if(!(allAirport.contains(it[1]))) allAirport.add(it[1]) }
 
-        var tree = arrayListOf<String>()
+        //티켓 중복 제거
+        val ticketsSet = HashSet<Array<String>>()
+        for (i in tickets)
+            ticketsSet.add(i)
+
+        var tree = ArrayList<String>()
         tree.add("ICN")
 
         //비대칭인 이진트리로 구현
@@ -28,11 +32,23 @@ class Solution19 {
                 ticketsSet.filter { tic ->
                     value.substring(value.length - 3) == tic.get(0)
                 }.forEach { item ->
-                    child.add(value + item.get(1))
+                    if(item.size == 0)
+                        tree.remove(value)
+                    else
+                        child.add(value + item.get(1))
                 }
             }
             tree = child
         }
+
+
+        allAirport.forEach { eachAirport ->
+            for(i in 0 until tree.size)
+                if (!(tree[i].contains(eachAirport)))
+                    tree.set(i, "")
+        }
+        tree.filter { it == "" }.forEach { tree.remove(it) }
+
         //정렬해서 알파벳 순으로 하나만 결과로
         var answertree = tree.sorted().get(0)
 
