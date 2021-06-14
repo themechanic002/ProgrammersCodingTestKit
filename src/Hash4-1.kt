d1d7d43bb31bd197b599825dab7df4a7685c3d76
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-14 13:42:43 +0900

    commit

--- src/Hash4-1.kt
+++ src/Hash4-1.kt
@@ -12,22 +12,22 @@ class Solution21 {
         var answer = ArrayList<Int>()
 
         val musicMap = HashMap<String, ArrayList<Int>>() // 근본이 되는 Map
-        val playsPerGenres = HashMap<String, Int>()
-        val playsList = ArrayList<HashMap<String, Int>>()
+        val playsPerGenres = HashMap<String, Int>() // <장르, 그 장르의 총 플레이 수 합계>가 들어있는 Map
+        val playsList = ArrayList<HashMap<String, Int>>() // 노래들의 순서가 인덱스인 배열. 각 인덱스에 따른 <장르, 플레이수>가 들어있음
 
+        //각 변수에 맞게 값 할당하기
         for (i in 0 until genres.size) {
-            if (musicMap.containsKey(genres[i])) {
+            playsList.add(hashMapOf(Pair(genres[i], plays[i])))
+            if (musicMap.containsKey(genres[i]))
                 musicMap.get(genres[i])?.add(plays[i])
-                playsList.add(hashMapOf(Pair(genres[i], plays[i])))
-            } else {
+            else
                 musicMap.put(genres[i], arrayListOf(plays[i]))
-                playsList.add(hashMapOf(Pair(genres[i], plays[i])))
-            }
         }
         musicMap.keys.forEach { eachKey ->
             playsPerGenres.put(eachKey, musicMap.get(eachKey)!!.sum())
         }
-
+        
+        // 가장 플레이 수가 많은 장르부터 answer에다가 최다 플레이 수와 두번째 플레이 수 노래의 인덱스들을 넣어줌.
         val sortedGenres = playsPerGenres.toList().sortedByDescending { it.second }
         sortedGenres.forEach { theGenre ->
             answer.add(
@@ -37,7 +37,9 @@ class Solution21 {
                     )
                 })
             )
+            // 만약 그 장르에 속한 노래가 두 개 이상이라면
             if (musicMap.get(theGenre.first)?.size != 1) {
+                // 한 장르에 최다 플레이 수를 가진 노래가 여러 개 있을 수 있으니 이미 answer에 넣은 거는 플레이 수를 -1로 바꿔버림
                 playsList.find {
                     it.containsValue(
                         musicMap.get(theGenre.first)?.sortedDescending()?.get(0)
@@ -53,7 +55,6 @@ class Solution21 {
                 )
             }
         }
-
         return answer
     }
 }
