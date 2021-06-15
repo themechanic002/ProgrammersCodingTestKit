adb47bce3128de0425f61b871556d978c98273b0
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-15 23:09:52 +0900

    commit

--- src/StringCompression.kt
+++ src/StringCompression.kt
@@ -1,34 +1,48 @@
 fun main() {
-    println(Solution28().solution("aabbaccc"))
+    println(Solution28().solution("bbaabaaaab"))
 }
 
 class Solution28 {
     fun solution(s: String): Int {
         var answer = ArrayList<String>()
+        answer.add(s)
 
         for (i in 1..(s.length) / 2) { //글자 한 집합의 길이
-            var one = ""
-            for (j in 0 until s.length - i) { //글자를 한칸씩 오른쪽으로 옮기면서 중복 찾기
-                var count = 0
-                val tempArray = ArrayList<String>()
-                var temp = s.substring(j)
-                while (temp[i - 1] != null) {
-                    tempArray.add(temp.substring(0, i))
-                    temp = temp.substring(i)
-                }
-                for (k in 0 until tempArray.size) {
-                    if (tempArray[k + 1] != null && tempArray[k] == tempArray[k + 1])
+            var one = s
+            var makingOne = ""
+            val tempArray = ArrayList<String>()
+            for (k in 0 until (one.length) / i) //tempArray에 글자 집합끼리 뭉쳐서 넣어놓기
+                tempArray.add(one.substring(i * k, (i * k) + i))
+            tempArray.add(one.substring((one.length / i) * i))
+
+
+            for (j in tempArray) { //한 집합마다
+                var count = 1
+                var index = tempArray.indexOf(j)
+                while (j != "") {
+                    if (index + 1 > tempArray.lastIndex) {
+                        break
+                    }
+                    if (tempArray[index] == tempArray[index + 1]) { //어떤 글자 집합이 바로 다음 글자 집합이랑 똑같다면
+                        index++
                         count++
-                    else {
-                        one = one + count.toString() + tempArray[k]
-                        if (tempArray[k + 1] != null)
-                            for (l in k until tempArray.size)
-                                one = one + tempArray[k]
+                    } else {
                         break
                     }
                 }
+                when (count > 1) { //똑같은 게 한 번이면 의미 없으니까 2번 이상일 때부터 숫자로 변환
+                    true -> {
+                        makingOne = makingOne + count + j
+                        for (m in 1 until count) {
+                            if (tempArray.indexOf(j) + m > tempArray.lastIndex)
+                                break
+                            tempArray.set(tempArray.indexOf(j) + m, "")
+                        }
+                    }
+                    false -> makingOne = makingOne + j
+                }
             }
-            answer.add(one)
+            answer.add(makingOne)
         }
 
         return answer.sortedBy { it.length }.first().length
