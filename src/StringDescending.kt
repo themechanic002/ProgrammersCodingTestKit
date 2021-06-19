af5f6159ce7715601cd686d016feab4e7e81e3f0
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-19 23:37:06 +0900

    commit

---
+++ src/StringDescending.kt
@@ -0,0 +1,12 @@
+class Solution43 {
+    fun solution(s: String): String {
+        val list = ArrayList<Char>()
+        var answer = ""
+        for(i in s)
+            list.add(i)
+        list.sortDescending()
+        for(i in list)
+            answer += i.toString()
+        return answer
+    }
+}
