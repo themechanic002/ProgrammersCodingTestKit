a1b92c0b34db6510838a03f0531ae46ab71effc1
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-20 22:42:36 +0900

    commit

---
+++ src/Tuple.kt
@@ -0,0 +1,17 @@
+class Solution47 {
+    fun solution(s: String): ArrayList<Int> {
+        val tupleArray = s.substring(2, s.lastIndex - 1).split("},{")
+        val tupleSet = HashSet<List<String>>()
+        for (i in tupleArray)
+            tupleSet.add(i.split(","))
+
+        var answer = ArrayList<Int>()
+
+        tupleSet.toList().sortedBy { it.size }.forEach { bigRange ->
+            for (i in 0 until bigRange.size)
+                if (!answer.contains(bigRange[i].toInt()))
+                    answer.add(bigRange[i].toInt())
+        }
+        return answer
+    }
+}
