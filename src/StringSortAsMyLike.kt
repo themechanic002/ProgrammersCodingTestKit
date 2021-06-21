b9da26693f1af823feec4a9206bff55e8496b1e1
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-21 17:13:05 +0900

    commit

--- src/StringSortAsMyLike.kt
+++ src/StringSortAsMyLike.kt
@@ -1,6 +1,3 @@
 class Solution51 {
-    fun solution(strings: Array<String>, n: Int): Array<String> {
-        var answer = arrayOf<String>()
-        return answer
-    }
+    fun solution(strings: Array<String>, n: Int): List<String> = strings.sortedWith(compareBy({ it.get(n) }, { it }))
 }
