db695258086945aed7dfba8fe4a399d60d5a6a39
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-21 17:17:03 +0900

    commit

---
+++ src/MenuRenewal.kt
@@ -0,0 +1,6 @@
+class Solution52 {
+    fun solution(orders: Array<String>, course: IntArray): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+        return answer
+    }
+}
