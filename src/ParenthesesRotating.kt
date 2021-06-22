3572279ada2269ff2c8802554e0be6f84c8299b2
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-22 21:08:03 +0900

    commit

--- src/ParenthesesRotating.kt
+++ src/ParenthesesRotating.kt
@@ -1,13 +1,12 @@
 import java.util.*
-import kotlin.collections.ArrayList
 
 class Solution53 {
     fun solution(s: String): Int {
         var count = 0
         var s2 = s
-        for(i in 0 until s.length){
-            if(isRight(s2))
-                count ++
+        for (i in 0 until s.length) {
+            if (isRight(s2))
+                count++
             s2 = s2.substring(1) + s2[0].toString()
         }
         return count
@@ -18,25 +17,25 @@ class Solution53 {
         val stack2 = Stack<Char>()
         val stack3 = Stack<Char>()
 
-        for(i in s){
-            when(i){
+        for (i in s) {
+            when (i) {
                 '(' -> stack1.push('(')
                 '{' -> stack2.push('{')
                 '[' -> stack3.push('[')
                 ')' -> {
-                    if(stack1.isEmpty())
+                    if (stack1.isEmpty())
                         return false
                     else
                         stack1.pop()
                 }
                 '}' -> {
-                    if(stack2.isEmpty())
+                    if (stack2.isEmpty())
                         return false
                     else
                         stack2.pop()
                 }
                 ']' -> {
-                    if(stack3.isEmpty())
+                    if (stack3.isEmpty())
                         return false
                     else
                         stack3.pop()
