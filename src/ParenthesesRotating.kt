518e4c35535138dffa6620f972a35df1ac31be25
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-22 21:02:00 +0900

    commit

--- src/ParenthesesRotating.kt
+++ src/ParenthesesRotating.kt
@@ -1,6 +1,42 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
 class Solution53 {
     fun solution(s: String): Int {
         var answer: Int = -1
         return answer
+    }
+
+    fun isRight(s: String): Boolean {
+        val stack1 = Stack<Char>()
+        val stack2 = Stack<Char>()
+        val stack3 = Stack<Char>()
+
+        for(i in s){
+            when(i){
+                '(' -> stack1.push('(')
+                '{' -> stack2.push('{')
+                '[' -> stack3.push('[')
+                ')' -> {
+                    if(stack1.isEmpty())
+                        return false
+                    else
+                        stack1.pop()
+                }
+                '}' -> {
+                    if(stack2.isEmpty())
+                        return false
+                    else
+                        stack2.pop()
+                }
+                ']' -> {
+                    if(stack3.isEmpty())
+                        return false
+                    else
+                        stack3.pop()
+                }
+            }
+        }
+        return stack1.isEmpty() && stack2.isEmpty() && stack3.isEmpty()
     }
 }
