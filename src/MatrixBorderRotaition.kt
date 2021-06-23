69727b42f0e3b4910ac04d085ca03a5c0d930075
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-23 13:23:30 +0900

    commit

--- src/MatrixBorderRotaition.kt
+++ src/MatrixBorderRotaition.kt
@@ -1,8 +1,7 @@
 fun main() {
     println(
         Solution48().solution(
-            3,
-            3,
+            3, 3,
             arrayOf(intArrayOf(1, 1, 2, 2), intArrayOf(1, 2, 2, 3), intArrayOf(2, 1, 3, 2), intArrayOf(2, 2, 3, 3))
         )
     )
@@ -18,8 +17,9 @@ class Solution48 {
                 matrix[i].add((j + 1) + (columns * i))
         }
 
-        for (i in queries)
+        for (i in queries) {
             matrix = rotate(matrix, i[0] - 1, i[1] - 1, i[2] - 1, i[3] - 1)
+        }
         return answer
     }
 
@@ -29,26 +29,26 @@ class Solution48 {
         for (i in 0 until matrix.size) {
             newMatrix.add(arrayListOf())
             for (j in 0 until matrix.get(i).size)
-                newMatrix[i].add((j + 1) + (matrix.get(i).size * i))
+                newMatrix[i].add(matrix[i][j])
         }
 
         val mins = ArrayList<Int>()
 
         for (i in 0 until x2 - x1) {
-            newMatrix[y1].set(x1 + i + 1, matrix[y1][x1 + i])
-            //newMatrix[y1][x1 + i + 1] = matrix[y1][x1 + i]
+            //newMatrix[y1].set(x1 + i + 1, matrix[y1][x1 + i])
+            newMatrix[y1][x1 + i + 1] = matrix[y1][x1 + i]
         }
         for (i in 0 until y2 - y1) {
-            newMatrix[y1 + i + 1].set(x2, matrix[y1 + i][x2])
-            //newMatrix[y1 + i + 1][x2] = matrix[y1 + i][x2]
+            //newMatrix[y1 + i + 1].set(x2, matrix[y1 + i][x2])
+            newMatrix[y1 + i + 1][x2] = matrix[y1 + i][x2]
         }
         for (i in 0 until x2 - x1) {
-            newMatrix[y2].set(x2 - i - 1, matrix[y2][x2 - i])
-            //newMatrix[y2][x2 - i - 1] = matrix[y2][x2 - i]
+            //newMatrix[y2].set(x2 - i - 1, matrix[y2][x2 - i])
+            newMatrix[y2][x2 - i - 1] = matrix[y2][x2 - i]
         }
         for (i in 0 until y2 - y1) {
-            newMatrix[y2 - i - 1].set(x1, matrix[y2 - i][x1])
-            //newMatrix[y2 - i - 1][x1] = matrix[y2 - i][x1]
+            //newMatrix[y2 - i - 1].set(x1, matrix[y2 - i][x1])
+            newMatrix[y2 - i - 1][x1] = matrix[y2 - i][x1]
         }
 
 
@@ -58,6 +58,8 @@ class Solution48 {
         }
         for (i in y1 + 1 until y2) {
             mins.add(newMatrix[i][x1])
+        }
+        for (i in y1 + 1 until y2) {
             mins.add(newMatrix[i][x2])
         }
 
