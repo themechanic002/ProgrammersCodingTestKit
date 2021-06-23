2ee8577db7fd28f48987094f120d6d414948493b
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-23 22:23:56 +0900

    commit

--- src/FibonacciNumber.kt
+++ src/FibonacciNumber.kt
@@ -3,4 +3,24 @@ class Solution59 {
         var answer = 0
         return answer
     }
+
+    fun fibonacci(n: Long): Long {
+
+        var first = 0L
+        var second = 1L
+        return when (n) {
+            1L -> first
+            2L -> second
+            else -> {
+                var temp = first + second
+                for (i in 3..n) {
+                    temp = first + second
+                    first = second
+                    second = temp
+                }
+                temp
+            }
+        }
+
+    }
 }
