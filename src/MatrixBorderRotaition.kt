e7456fc036029cca69e39692cfb3c74e1a9b5359
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-23 13:06:43 +0900

    commit

--- src/MatrixBorderRotaition.kt
+++ src/MatrixBorderRotaition.kt
@@ -1,43 +1,61 @@
 class Solution48 {
-    fun solution(rows: Int, columns: Int, queries: Array<IntArray>): IntArray {
-        var answer = intArrayOf()
+    var answer = ArrayList<Int>()
+    fun solution(rows: Int, columns: Int, queries: Array<IntArray>): ArrayList<Int> {
         var matrix = ArrayList<ArrayList<Int>>()
-        var index = 0
         for (i in 0 until rows) {
             matrix.add(arrayListOf())
-            for (j in 0 until columns) {
-                index++
-                matrix[i].add(index)
-            }
-        }
-        queries.forEach {
-
-            val rec = ArrayList<Int>()
-            for (i in matrix[it.get(0) - 1][it.get(1) - 1] until matrix[it.get(0) - 1][it.get(3)] - 1) {
-                rec.add(i)
-            }
-            for (i in matrix[it.get(0) - 1][it.get(3) - 1] until matrix[it.get(2) - 1][it.get(3) - 1] step columns) {
-                rec.add(i)
-            }
-            for (i in matrix[it.get(2) - 1][it.get(3) - 1] downTo matrix[it.get(2) - 1][it.get(0) + 1 - 1]) {
-                rec.add(i)
-            }
-            for (i in matrix[it.get(2) - 1][it.get(0) - 1] downTo matrix[it.get(0) + 1 - 1][it.get(1) - 1] step columns) {
-                rec.add(i)
-            }
-
-            val first = rec[0]
-            for(i in 0 until rec.size){
-                if(i == rec.size - 1)
-                    rec.set(i, first)
-                else
-                    rec.set(i, rec[i + 1])
-            }
-
-
+            for (j in 0 until columns)
+                matrix[i].add((j + 1) + (columns * i))
         }
 
-
+        for (i in queries) {
+            matrix = rotate(matrix, i[0] - 1, i[1] - 1, i[2] - 1, i[3] - 1)
+        }
         return answer
+
+    }
+
+    fun rotate(matrix: ArrayList<ArrayList<Int>>, y1: Int, x1: Int, y2: Int, x2: Int): ArrayList<ArrayList<Int>> {
+
+        val newMatrix = ArrayList<ArrayList<Int>>()
+        for (i in 0 until matrix.size) {
+            newMatrix.add(arrayListOf())
+            for (j in 0 until matrix.get(i).size) {
+                newMatrix[i].add((j + 1) + (matrix.get(i).size * i))
+            }
+        }
+
+        val mins = ArrayList<Int>()
+
+        for (i in 0 until x2 - x1) {
+            //newMatrix[y1].set(x1 + i + 1, matrix[y1][x1 + i])
+            newMatrix[y1][x1 + i + 1] = matrix[y1][x1 + i]
+        }
+        for (i in 0 until y2 - y1) {
+            //newMatrix[y1 + i + 1].set(x2, matrix[y1 + i][x2])
+            newMatrix[y1 + i + 1][x2] = matrix[y1 + i][x2]
+        }
+        for (i in 0 until x2 - x1) {
+            //newMatrix[y2].set(x2 - i - 1, matrix[y2][x2 - i])
+            newMatrix[y2][x2 - i - 1] = matrix[y2][x2 - i]
+        }
+        for (i in 0 until y2 - y1) {
+            //newMatrix[y2 - i - 1].set(x1, matrix[y2 - i][x1])
+            newMatrix[y2 - i - 1][x1] = matrix[y2 - i][x1]
+        }
+
+
+        for (i in x1..x2) {
+            mins.add(newMatrix[y1][i])
+            mins.add(newMatrix[y2][i])
+        }
+        for (i in y1 + 1 until y2) {
+            mins.add(newMatrix[i][x1])
+            mins.add(newMatrix[i][x2])
+        }
+
+        answer.add(mins.sorted().get(0))
+
+        return newMatrix
     }
 }
