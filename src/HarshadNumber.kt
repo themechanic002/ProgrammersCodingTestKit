269252196c7012b868ae7b381b3acc2941e45b17
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-23 15:28:43 +0900

    commit

--- src/HarshadNumber.kt
+++ src/HarshadNumber.kt
@@ -1,3 +1,3 @@
 class Solution56 {
-    fun solution(x: Int): Boolean = x % x.toString().toCharArray().map { it.code - '0'.code }.sum() == 0
+    fun solution(x: Int): Boolean = x % x.toString().map { it.code - '0'.code }.sum() == 0
 }
