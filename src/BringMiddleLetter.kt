901d8907614108dda28bc03f1b9e97c0d5f1ad1c
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-24 17:19:22 +0900

    commit

--- src/BringMiddleLetter.kt
+++ src/BringMiddleLetter.kt
@@ -1,3 +1,7 @@
+fun main() {
+    Solution39().solution("123456")
+}
+
 class Solution39 {
     fun solution(s: String): String =
         if (s.length % 2 == 0) (s[s.length / 2 - 1].toString() + s[s.length / 2]) else s[s.length / 2].toString()
