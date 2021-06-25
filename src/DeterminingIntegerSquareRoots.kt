2ef40cc27325bd1eec27ed1a51d5b4121542c944
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-25 22:23:00 +0900

    commit

--- src/DeterminingIntegerSquareRoots.kt
+++ src/DeterminingIntegerSquareRoots.kt
@@ -2,8 +2,9 @@ import kotlin.math.*
 
 class Solution61 {
     fun solution(n: Long): Long {
-        var answer: Long = 0
-        return answer
+        if(isSquareRoot(n.toDouble()))
+            return (sqrt(n.toDouble()) + 1.0).pow(2).toLong()
+        else return -1L
     }
     fun isSquareRoot(n: Double): Boolean = sqrt(n).toInt().toDouble().pow(2) == n
 }
