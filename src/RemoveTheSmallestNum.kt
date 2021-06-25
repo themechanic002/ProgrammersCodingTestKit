6470beab4d3f2cbe7a14504f54af82c532501ccd
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-25 22:36:23 +0900

    commit

--- src/RemoveTheSmallestNum.kt
+++ src/RemoveTheSmallestNum.kt
@@ -1,6 +1,12 @@
 class Solution62 {
     fun solution(arr: IntArray): IntArray {
-        var answer = intArrayOf()
-        return answer
+        val arr2 = arr.copyOf().sorted().toList()
+        val smallest = arr2.first()
+        val answer = ArrayList<Int>()
+        arr.forEach {
+            if(it != smallest)
+                answer.add(it)
+        }
+        return answer.toIntArray()
     }
 }
