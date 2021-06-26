a4a3bf15565bc4facad9363c3861bb60071dbe9d
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-26 20:25:57 +0900

    commit

--- src/ReversingNaturalToArr.kt
+++ src/ReversingNaturalToArr.kt
@@ -1,3 +1,3 @@
 class Solution65 {
-    fun solution(n: Long): List<Char> = n.toString().reversed().toList()
+    fun solution(n: Long): List<Int> = n.toString().reversed().map{ it.toInt() - '0'.toInt() }.toList()
 }
