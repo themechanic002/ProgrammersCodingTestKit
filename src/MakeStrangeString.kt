e6a4bd00b71f73c2b5300175e14e110f425b697f
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-26 20:18:24 +0900

    commit

--- src/MakeStrangeString.kt
+++ src/MakeStrangeString.kt
@@ -1,6 +1,21 @@
 class Solution64 {
     fun solution(s: String): String {
         var answer = ""
+        var index = 0
+        for(i in s){
+            if(i == ' '){
+                answer += " "
+                index = 0
+            }
+            else if(index % 2 == 0){
+                answer += i.toString().toUpperCase()
+                index++
+            }
+            else if(index % 2 == 1){
+                answer += i.toString().toLowerCase()
+                index++
+            }
+        }
         return answer
     }
 }
