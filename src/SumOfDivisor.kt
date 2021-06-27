3ffa9cecc8a5d753aa68bdc63e75db6a84f9ca85
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-27 22:31:41 +0900

    commit

--- src/SumOfDivisor.kt
+++ src/SumOfDivisor.kt
@@ -1,10 +1,9 @@
 class Solution66 {
     fun solution(n: Int): Int {
         val divisor = ArrayList<Int>()
-        for(i in 1 .. n){
+        for(i in 1 .. n)
             if(n % i == 0)
                 divisor.add(i)
-        }
         return divisor.sum()
     }
 }
