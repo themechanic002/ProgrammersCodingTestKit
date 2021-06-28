cb45fdc77d2c04ab27136c901f93755af07d3ec5
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-28 23:57:32 +0900

    commit

--- src/HandlingString.kt
+++ src/HandlingString.kt
@@ -1,8 +1,9 @@
 class Solution67 {
-    fun solution(s: String): Boolean {
+    fun solution(s: String): Boolean = (s.length == 4||s.length == 6) && (s.all { it>='0' && it<='9' })
+    /*{
         if(s.length == 4 || s.length == 6)
             if(s.all { it >= '0' && it <= '9' })
                 return true
         return false
-    }
+    }*/
 }
