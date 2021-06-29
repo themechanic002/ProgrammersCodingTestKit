38d2bb5cd326b5d3aa90580bee1c356b0d125fe0
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-29 23:28:04 +0900

    commit

--- src/FindingKim.kt
+++ src/FindingKim.kt
@@ -1,5 +1,5 @@
 class Solution69 {
     fun solution(seoul: Array<String>): String {
-        return "김서방은 " + seoul.indexOf(seoul.find { it == "Kim" }) + "에 있다"
+        return "김서방은 " + seoul.indexOf("Kim") + "에 있다"
     }
 }
