cc95633e5641c3ae744e38fe6352e4dd995f4702
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-29 23:22:17 +0900

    commit

--- src/DescendingInt.kt
+++ src/DescendingInt.kt
@@ -1,5 +1,5 @@
 class Solution68 {
     fun solution(n: Long): Long {
-        return n.toString().toCharArray().sortedDescending().toString().toLong()
+        return String(n.toString().toCharArray().sortedDescending().toCharArray()).toLong()
     }
 }
