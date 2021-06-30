85e221c9b6902f929029d54d670c9556cde16991
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-06-30 22:45:51 +0900

    commit

--- src/CollatzGuess.kt
+++ src/CollatzGuess.kt
@@ -3,13 +3,12 @@ class Solution71 {
         var n = num
         var count = 0
         while (n != 1) {
-            n = collatz(n)
-            count++
             if (count == 500)
                 return -1
+            n = collatz(n)
+            count++
         }
         return count
     }
-
     fun collatz(num: Int): Int = if (num % 2 == 0) num / 2 else num * 3 + 1
 }
