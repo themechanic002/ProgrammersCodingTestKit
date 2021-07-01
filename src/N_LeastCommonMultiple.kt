f3f314d8c049e851084ce667e266959dd88c9929
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-01 21:30:23 +0900

    commit

--- src/N_LeastCommonMultiple.kt
+++ src/N_LeastCommonMultiple.kt
@@ -1,6 +1,13 @@
 class Solution72 {
     fun solution(arr: IntArray): Int {
-        var answer = 0
-        return answer
+        var sortedArr = arr.sortedDescending()
+        var index = sortedArr.first()
+        while (true){
+            if(sortedArr.all { it % index == 0 }){
+
+            }
+        }
+
+        return index
     }
 }
