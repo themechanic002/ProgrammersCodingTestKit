281d601792de5a9d6a21f69e98fbce82b954d9ad
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-01 21:48:12 +0900

    commit

--- src/N_LeastCommonMultiple.kt
+++ src/N_LeastCommonMultiple.kt
@@ -1,13 +1,8 @@
-fun main() {
-    println(Solution72().solution(intArrayOf(2, 6, 8, 14)))
-}
-
 class Solution72 {
     fun solution(arr: IntArray): Int {
         val sortedArr = arr.sorted()
         var index = sortedArr.last()
         val biggest = index
-        println(index)
         while (true) {
             if (sortedArr.all { index % it == 0 })
                 return index
