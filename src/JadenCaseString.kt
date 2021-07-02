2c9c60a025439039e0883747d2ab83a1c587c06c
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-02 22:22:50 +0900

    commit

--- src/JadenCaseString.kt
+++ src/JadenCaseString.kt
@@ -2,9 +2,9 @@ class Solution73 {
     fun solution(s: String): String {
         var answer = ""
         s.forEach {
-            if(it == ' ')
+            if (it == ' ')
                 answer += " "
-            else if(answer.isBlank() || answer.last() == ' ')
+            else if (answer.isBlank() || answer.last() == ' ')
                 answer += it.uppercase()
             else
                 answer += it.lowercase()
