134fee82b66b270a81bb3abb00eec177096c0434
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-02 22:14:02 +0900

    commit

--- src/JadenCaseString.kt
+++ src/JadenCaseString.kt
@@ -1,19 +1,16 @@
 class Solution73 {
     fun solution(s: String): String {
-        val arr = s.replace("\\s+".toRegex(), " ").split(" ")
         var answer = ""
-        arr.forEach {
-            answer += jadenCase(it)
-            if (arr.indexOf(it) != arr.lastIndex)
+        s.forEach {
+            if(it == ' ')
                 answer += " "
+            else if(it == s.first())
+                answer += it.uppercase()
+            else if(s[s.indexOf(it) - 1] == ' ')
+                answer += it.uppercase()
+            else
+                answer += it.lowercase()
         }
         return answer
-    }
-
-    fun jadenCase(s: String): String {
-        if (s.length == 1)
-            return s.uppercase()
-        else
-            return s[0].uppercase() + s.substring(1).lowercase()
     }
 }
