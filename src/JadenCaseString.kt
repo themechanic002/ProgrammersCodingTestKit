1145f756d5f137b7cb0136aa21a670a725669519
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-02 21:46:31 +0900

    commit

--- src/JadenCaseString.kt
+++ src/JadenCaseString.kt
@@ -1,6 +1,12 @@
 class Solution73 {
     fun solution(s: String): String {
+        val arr = s.split(" ")
         var answer = ""
+        arr.forEach {
+            answer += it[0].uppercase()
+            answer += it.substring(1).lowercase()
+            answer += " "
+        }
         return answer
     }
 }
