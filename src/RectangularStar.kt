81c54aa7bce03f7bd56500216197e0641a303f67
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-03 21:53:46 +0900

    commit

--- src/RectangularStar.kt
+++ src/RectangularStar.kt
@@ -1,7 +1,11 @@
+class Solution74 {
+
+}
+
 fun main(args: Array<String>) {
     val (a, b) = readLine()!!.split(' ').map(String::toInt)
-    for(i in 0 until b){
-        for(j in 0 until a)
+    for (i in 0 until b) {
+        for (j in 0 until a)
             print("*")
         println()
     }
