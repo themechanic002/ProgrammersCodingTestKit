f42c8aa88bdfd5e05c2918807d416c45f68c3bd2
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-04 23:09:00 +0900

    commit

---
+++ src/GCDandLCM.kt
@@ -0,0 +1,18 @@
+import java.lang.Math.*
+
+class Solution75 {
+    fun solution(n: Int, m: Int): IntArray {
+        var answer = intArrayOf()
+        return answer
+    }
+    
+
+    fun gcd(a: Int, b: Int): Int {
+        var maximum = max(a, b)
+        var minimum = min(a, b)
+        if (minimum == 0)
+            return max(a, b)
+        else
+            return gcd(minimum, maximum % minimum)
+    }
+}
